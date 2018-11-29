120045579f728c3f2117e1465ec84579ddeeddba
Kangchuijing <1806993519@qq.com>
2018-11-29 11:28:08 +0800

    完成用户信息查询接口~好轻松啊

--- server.js
+++ server.js
@@ -65,12 +65,53 @@ app.post('/user/login', function (req, res, next) {
 });
 
 
-app.use(function (req, res, next) {
-    res.send('404');
-});
-
+// 展示用户管理界面
 app.get('/user/userManage', function (req, res) {
-    MongoClient.connect(url, { useNewUrlParser: true }, function (error, data) { })
+    var page = parseInt(req.body.page) || 1;   // 展示当前所在的页数，如果是第一次或者是未传值，默认为第一页
+    var pageSize = parseInt(req.body.pageSize) || 5;  // 每一页显示数据的条数，如果未传值，默认为5条数据
+    var result = {};    // 返回给前端的信息
+
+    // test 测试数据
+    // res.send('我是数据');
+    // return;
+
+    MongoClient.connect(url, { useNewUrlParser: true }, function (error, client) {
+        if (error) {
+            console.log('数据库连接失败');
+            result.code = -1;
+            result.msg = '数据库连接失败';
+            res.json(result);
+            return;
+        }
+
+        // 如果数据库连接成功，此时对数据库进行操作
+        var db = client.db('project');
+        // 对用户列表信息进行分页查询
+        db.collection('users').find().limit(pageSize).skip(pageSize * (page - 1)).toArray(function (error, data) {
+            if (error) {   // 如果查询过程中出现了错误
+                console.log('数据查询失败');
+                result.code = -1;
+                result.msg = '数据查询失败';
+            } else if (data.length <= 0) {  // 如果数据库中没有数据
+                console.log('数据库中无数据');
+                result.code = -1;
+                result.msg = '数据库中没有更多数据了';
+            } else {    // 此时查询到了数据
+                console.log('数据查询成功');
+                result.code = 0;
+                result.msg = '数据查询成功';
+                result.data = data;
+            }
+            res.json(result);   // 将数据响应给前端
+            client.close();     // 关闭数据库的连接
+        });
+
+    })
+
+    // 404错误页面是要放在最后的
+    app.use(function (req, res, next) {
+        res.send('404');
+    });
 });
 
 app.listen(3000);
