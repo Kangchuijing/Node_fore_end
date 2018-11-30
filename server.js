8a7de53d7cbaae3502623f19578c5841e54e5f2f
Kangchuijing <1806993519@qq.com>
2018-11-30 10:01:51 +0800

    完成用户信息的搜素功能，用户信息修改有些小bug，待修复

--- server.js
+++ server.js
@@ -261,6 +261,200 @@ app.post('/user/delete', function (req, res) {
     });
 });
 
+// 更新用户信息
+app.post('/user/update', function (req, res, next) {
+    console.log(req.body);
+    // 定义一个数组检验数据的完整型
+    var arr = ['name', 'nickname', 'password', 'sex', 'isAdmin', 'power'];
+    result = {};
+    for (var key in req.body) {
+        if (arr.indexOf(key) == -1) {
+            console.log('数据不完整');
+            result.code = -1;
+            result.msg = '请提交完整的数据';
+            res.json(result);
+            return;
+        }
+    }
+    // 如果数据是完整的
+    MongoClient.connect(url, { useNewUrlParser: true }, function (error, client) {
+        if (error) {
+            result.code = -1;
+            result.msg = '数据库连接失败';
+            return;
+        }
+
+        var db = client.db('project');
+        // 首先检验用户更改的用户名是否存在，如果用户名在数据库中已经有了，那么将不会对用户名做修改
+        async.waterfall([
+            function (cb) {
+                db.collection('users').find({
+                    name: req.body.name
+                }).toArray(function (error, data) {
+                    if (error) {
+                        cb('操作失败');
+                    } else if (data.length >= 1) {   // 说明数据库中已经有这个用户名，那么将不会更新这个用户名
+                        cb(null, true);
+                    } else {        // 如果用户更改的用户名数据库中没有，那么允许用户进行更改
+                        cb(null, false);
+
+                    }
+                });
+            }, function (flag, cb) {
+                if (flag) {   // 如果用户名在数据库中已经存在，那么只会更新其他数据
+                    db.collection('users').updateOne({
+                        _id: ObjectId(req.body['_id'])
+                    }, {
+                            $set: {
+                                nickname: req.body.nickname,
+                                password: req.body.pasword,
+                                sex: req.body.sex,
+                                isAdmin: req.body.isAdmin,
+                                power: req.body.power
+                            }
+                        }, function (error) {
+                            if (error) {
+                                console.log('数据更新失败1');
+                                cb('数据更新失败');
+                            } else {
+                                console.log('数据更新成功1');
+                                cb(null);
+                            }
+                        });
+
+                } else {
+                    // 如果用户名在数据库中不存在，那么就更新全部数据
+                    db.collection('users').updateOne({
+                        _id: ObjectId(req.body['_id'])
+                    }, {
+                            $set: {
+                                name: req.body.name,
+                                nickname: req.body.nickname,
+                                password: req.body.password,
+                                sex: req.body.sex,
+                                isAdmin: req.body.isAdmin,
+                                power: req.body.power
+                            }
+
+                        }, function (error) {
+
+                            if (error) {
+                                console.log('数据更新失败2', error);
+                                cb('数据更新失败');
+                            } else {
+                                console.log('数据更新成功2');
+                                cb(null);
+                            }
+                        });
+                }
+            }
+
+        ], function (error, results) {
+            if (error) {   // 异步操作中出现错误
+                result.code = -1;
+                result.msg = error;
+            } else {
+                result.code = 0;
+                result.msg = '数据更新成功';
+            }
+            // 将数据响应至前端
+            res.json(result);
+            client.close();
+        });
+    });
+
+});
+
+// 获取用户个人信息接口
+app.get('/userInfo', function (req, res, next) {
+    var id = ObjectId(req.query.id);   // 获取用户传递过来的id
+    var result = {};
+    if (!id) {    // 如果id信息不存在
+        result.code = -1;
+        result.msg = '未选择要修改的数据';
+        res.json(result);
+        return;
+    }
+    // 如果id信息存在，就去连接数据库进行操作
+    MongoClient.connect(url, { useNewUrlParser: true }, function (error, client) {
+        if (error) {
+            console.log('数据库连接失败');
+            result.code = -1;
+            result.msg = '操作有误';
+            res.json(result);
+            return;
+        }
+        // 连接数据库成功
+        var db = client.db('project');
+        db.collection('users').find({
+            _id: id
+        }).toArray(function (error, data) {
+            if (error) {        // 当查找出错时
+                result.code = -1;
+                result.msg = '操作失败';
+            } else if (data.length <= 0) {  //  当没有查找到用户信息时
+                result.code = -1;
+                result.msg = '操作失败'
+            } else {    // 查找成功
+                result.code = 0;
+                result.msg = '查找成功';
+                result.data = data;
+                console.log(data);
+            }
+            // 将信息响应给前端
+            res.json(result);
+            client.close();
+        });
+
+    });
+
+});
+
+// 用户信息搜索接口
+app.get('/user/search', function (req, res, next) {
+    var result = {};    // 响应给前端的信息
+    if (!req.query.nickname) {
+        result.code = 1;
+        result.msg = '请输入查询条件';
+        res.json(result);
+        return;
+    }
+    // 当用户提交了查询条件才进行查询
+    MongoClient.connect(url, { useNewUrlParser: true }, function (error, client) {
+        if (error) {    // 如果数据库连接出错
+            console.log('数据库连接失败');
+            result.code = -1;
+            result.msg = '数据库连接失败';
+            res.json(result);
+            return;
+        }    // 如果数据库连接成功
+        var db = client.db('project');
+        var query = new RegExp(req.query.nickname);
+        db.collection('users').find({
+            nickname: query
+        }).toArray(function (error, data) {
+            if (error) {    // 如果查找出错
+                console.log('模糊查询失败');
+                result.code = -1;
+                result.msg = '模糊查询失败';
+            } else if (data.length <= 0) {  // 如果没有找到数据
+                console.log('没有更多数据了');
+                result.code = -1;
+                result.msg = '没有更多数据了';
+            } else {    // 匹配到了一条及以上的数据
+                console.log('模糊匹配成功');
+                result.code = 0;
+                result.msg = '查找成功';
+                result.data = data;
+                console.log(result);
+            }
+            // 将结果响应给前端
+            res.json(result);
+            client.close();
+        });
+    });
+});
+
 // 404错误页面是要放在最后的，将文件读取出来发送给用户
 app.get('/404', function (req, res, next) {
     res.sendFile(path.join(__dirname, './404.html'));
